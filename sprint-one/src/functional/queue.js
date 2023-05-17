fc43268e0927ba3b05d693c69803bd5280ba3dd1
Loren <loren.johnson99@yahoo.com>
2023-05-17 13:26:10 -0700

    completed functional patterns

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // get object size, is the index at which to add value
+    var size = someInstance.size();
+    if (value === 'b') {
+      debugger;
+    }
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    var value = storage[0];
+    delete storage[0];
+
+    // update indices, since first index 0 is gone
+    var size = someInstance.size();
+    for (var i = 0; i < size; i++) {
+      storage[i] = storage[i + 1];
+    }
+    delete storage[size];
+
+    return value;
   };
 
   someInstance.size = function() {
+    return (Object.keys(storage)).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+
   // Implement the methods below
   someInstance.push = function(value) {
+    // add value to storage object with subsequent numeric key
+    var size = someInstance.size();
+    storage[size] = value;
+    // find last index in storage object (use size);
   };
 
   someInstance.pop = function() {
+    var size = someInstance.size();
+    var value = storage[size - 1]
+    delete storage[size - 1];
+    return value;
   };
 
   someInstance.size = function() {
+    return (Object.keys(storage)).length;
   };
 
   return someInstance;
