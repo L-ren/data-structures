a26c77853ed9586f6c730b3f088a99334eac5189
Loren <loren.johnson99@yahoo.com>
2023-05-18 18:12:31 -0700

    saving progress at end of day

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me //original value = null
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newborn = Tree(value);
+  this.children.push(newborn);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target) { //use recursion
+  //we're passed a target value
+  //declare something to contain boolean, which we return later
+  let containsValue;
+  //look at value stored current node (which starts at newTree,which is the root node))
+  //if it matches target, return true
+  if (this.value === target) {
+    return true;
+  }
+  //check if currentnode has children. if current node has children,
+  if(this.children){
+      //invoke forEach on each childNode
+      this.children.forEach(function(child) { //what kind of data structure is this.children?
+        //callback function forEach is contains(child???)
+        child.contains(target);
+      });
+  } //if no children - do nothing
+
+  return false;
+
 };
 
 
